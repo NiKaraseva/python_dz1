134f82a3ccb36c16ee075712539a647476e85dda
nikakaraseva <nikarasevas@gmail.com>
2023-07-17 08:58:12 +0300

    all done

---
+++ task_7.py
@@ -0,0 +1,38 @@
+# Пользователь вводит число от 1 до 999. Используя операции с числами
+# сообщите что введено: цифра, двузначное число или трёхзначное число.
+# Для цифры верните её квадрат, например 5 - 25
+# Для двузначного числа произведение цифр, например 30 - 0
+# Для трёхзначного числа его зеркальное отображение, например 520 - 25
+# Если число не из диапазона, запросите новое число
+# Откажитесь от магических чисел
+# В коде должны быть один input и один print
+
+
+number = int(input("Введите число: "))
+
+message = ""
+
+MIN_VALUE = 1
+MAX_VALUE = 999
+ONE_DIGIT = 10
+TWO_DIGIT = 100
+
+if MIN_VALUE <= number <= MAX_VALUE:
+    if number < ONE_DIGIT:
+        result = number ** 2
+        message = f"Квадрат цифры {number} = {result}"
+    elif number < TWO_DIGIT:
+        first_digit = number // 10
+        second_digit = number % 10
+        result = first_digit * second_digit
+        message = f"Произведение двузначного числа {number} = {result}"
+    else:
+        # result = int(str(number)[::-1])
+        first_digit = number // 100
+        second_digit = (number // 10) % 10
+        third_digit = number % 10
+        message = f"Зеркальное отображение трёхзначного числа {number} = {third_digit, second_digit, first_digit}"
+else:
+    message = f"Введено некоректное значение, введите число от 1 до 999"
+
+print(message)
---
+++ task_9.py
@@ -0,0 +1,16 @@
+# Выведите в консоль таблицу умножения от 2х2 до 9х10 как на школьной тетрадке
+
+COLOUMNS = 4
+ROWS = 2
+
+for i in range(ROWS):
+    for j in range(2, 11):
+        line = ""
+        for k in range(COLOUMNS):
+            table_num = k * ROWS + i + 2
+            if table_num > 9:
+                break
+            line += f"{table_num} x {j} = {table_num * j}\t   "
+        print(line)
+    print("–––––––––––––––––––––––––––")
+
